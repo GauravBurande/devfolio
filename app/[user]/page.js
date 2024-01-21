17f822d7aec13de1edc45058dd7d7f90cf83c27b
gaurav <burandegaurav8899@gmail.com>
2024-01-21 10:40:43 +0530

    project section done

--- app/[user]/page.js
+++ app/[user]/page.js
@@ -1,14 +1,16 @@
+import MobileNavSheet from '@/components/MobileNavSheet';
 import { ModeToggle } from '@/components/ThemeToggle';
-import { Icons } from '@/components/icons';
+import { socialIcons } from '@/libs/constants';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react'
 
 const user = {
     "name": "John Doe",
-    "headline": "I'm a Full Stack Engineer and Opensource Contributor",
-    "bio": "Passionate web developer with a love for clean code and creating awesome digital experiences, that you will like and everyone will like!",
-    "workExperience": [
+    "helloText": `👋 Hey there, I'm John Doe`,
+    "headline": "A Full Stack Engineer and Opensource Contributor",
+    "about": "Passionate web developer with a love for clean code and creating awesome digital experiences, that you will like and everyone will like!",
+    "work": [
         {
             "position": "Front-end Developer",
             "company": "Tech Innovations Inc.",
@@ -27,16 +29,32 @@ const user = {
     "projects": [
         {
             "title": "E-commerce Platform",
+            "role": "developer",
             "description": "Built a responsive e-commerce platform with user authentication and product management.",
-            "technologies": ["TailwindCSS", "NextJS", "MongoDB"],
+            "stack": ["TailwindCSS", "NextJS", "MongoDB"],
             "url": "https://example.com/ecommerce"
         },
         {
             "title": "Portfolio Website",
+            "role": "designer",
             "description": "Designed and developed a personal portfolio website to showcase skills and projects.",
-            "technologies": ["HTML", "CSS", "JavaScript"],
+            "stack": ["HTML", "CSS", "JavaScript"],
             "url": "https://example.com/portfolio"
-        }
+        },
+        {
+            "title": "Blog Platform",
+            "role": "maintainer",
+            "description": "Created a dynamic blog platform with user accounts and commenting functionality.",
+            "stack": ["React", "Node.js", "Express", "MongoDB"],
+            "url": "https://example.com/blog"
+        },
+        {
+            "title": "Social Media Dashboard",
+            "role": "developer",
+            "description": "Developed a social media dashboard with real-time updates and interactive data visualizations.",
+            "stack": ["Vue.js", "Firebase", "Chart.js"],
+            "url": "https://example.com/social-dashboard"
+        },
     ],
     "skills": ["MongoDB", "TailwindCSS", "JavaScript", "React", "Node.js", "NextJS", "Git"],
     "socials": [
@@ -59,33 +77,24 @@ const user = {
     ]
 }
 
-const socialIcons = {
-    email: <><Icons.email className="h-5 w-5 hover:text-primary" /></>,
-    linkedin: <Icons.linkedin className="h-5 w-5 hover:text-primary" />,
-    github: <Icons.gitHub className="h-4 w-4 hover:text-primary" />,
-    twitter: <Icons.twitter className="h-4 w-4 hover:text-primary" />,
-}
+const links = [];
 
-const links = [
-    {
-        href: "/#skills",
-        label: "Skills",
-    },
-    {
-        href: "/#projects",
-        label: "Projects",
-    },
-    {
-        href: "/#work",
-        label: "Work",
-    },
-];
+// Function to add a link to the 'links' array if the property exists in the 'user' object
+const addLink = (label, href) => {
+    if (user[label] && user[label].length > 0) {
+        links.push({ label, href });
+    }
+};
+
+addLink('skills', '#skills');
+addLink('projects', '#projects');
+addLink('work', '#work');
 
 const portfolio = ({ params }) => {
 
-    console.log("slug: " + params.user);
+    // console.log("slug: " + params.user);
     return (
-        <main>
+        <main className='pb-20'>
             <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
                 <nav
                     className="container flex items-center justify-between md:px-16 py-3 mx-auto"
@@ -106,7 +115,7 @@ const portfolio = ({ params }) => {
                                     width={32}
                                     height={32}
                                 />
-                                <span className="font-bold">{user.name}</span>
+                                <h1 className="font-bold">{user.name}</h1>
                             </Link>
                         </div>
                         <div className="hidden sm:flex justify-center gap-6 items-center">
@@ -114,7 +123,7 @@ const portfolio = ({ params }) => {
                                 <Link
                                     href={link.href}
                                     key={link.href}
-                                    className="transition-colors hover:text-foreground text-foreground/70"
+                                    className="transition-colors capitalize hover:text-foreground text-foreground/70"
                                     title={link.label}
                                 >
                                     {link.label}
@@ -132,11 +141,65 @@ const portfolio = ({ params }) => {
                             ))
                         }
                         <ModeToggle />
+                        <MobileNavSheet socials={user.socials} links={links} />
                     </div>
                 </nav>
             </header>
-            <section></section>
-            <footer></footer>
+            <section className="relative  pt-28 md:pt-40 pb-60 md:pb-52">
+                <div className="absolute inset-0 bg-[url(/graphic.webp)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
+                <div className="flex flex-col items-center justify-center text-center space-y-12 lg:space-y-8 relative mx-auto">
+                    <p className={`font-bold text-5xl leading-tight lg:leading-normal sm:text-6xl md:text-7xl px-5 md:px-7 lg:px-10`}>
+                        {user.helloText}
+                    </p>
+                    <h2 className='text-center text-4xl'>
+                        {user.headline}
+                    </h2>
+                    <h3 className='text-xl max-w-4xl px-5 text-center'>
+                        {user.about}
+                    </h3>
+                </div>
+            </section>
+            <section className='container mt-10'>
+                <h3 className='text-lg mb-14 font-semibold'>Projects</h3>
+                <div className='flex flex-col gap-16 items-start'>
+                    {user.projects.map(project => (
+                        <div key={project.title}>
+                            <div className='flex items-center gap-5'>
+                                <div className='bg-accent p-3 rounded-md border-2'>
+                                    <Image
+                                        src={'/project.svg'}
+                                        alt={`${project.title}`}
+                                        className="w-6"
+                                        priority={true}
+                                        width={32}
+                                        height={32}
+                                    />
+                                </div>
+                                <div className='flex flex-col gap-1'>
+                                    <div className='flex items-center gap-3'>
+                                        <p className='font-semibold text-foreground/75 text-lg'>{project.title}</p>
+                                        <p className='bg-primary/10 px-2 py-[2px] text-xs border-2 rounded-md'>{project.role}</p>
+                                        <Link className='hover:text-primary/80' href={project.url}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-external-link"><path d="M15 3h6v6" /><path d="M10 14 21 3" /><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" /></svg>
+                                        </Link>
+                                    </div>
+                                    <div className='flex items-center gap-4'>
+                                        {project.stack.map(tech => (
+                                            <p className='bg-primary/30 border-2 px-3 py-0.5 text-xs rounded-md' key={tech}>
+                                                {tech}
+                                            </p>
+                                        ))}
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className=' mt-3'>
+                                {project.description}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </section>
         </main>
     )
 }
---
+++ libs/constants.js
@@ -0,0 +1,8 @@
+const { Icons } = require("@/components/icons");
+
+export const socialIcons = {
+    email: <Icons.email className="h-5 w-5 hover:text-primary" />,
+    linkedin: <Icons.linkedin className="h-5 w-5 hover:text-primary" />,
+    github: <Icons.gitHub className="h-5 w-5 hover:text-primary" />,
+    twitter: <Icons.twitter className="h-4 w-4 hover:text-primary" />,
+}
