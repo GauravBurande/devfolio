6d760d4dcb9fa0d488d216b1463b27c96c691ae1
gaurav <burandegaurav8899@gmail.com>
2024-03-01 15:16:09 +0530

    fix: correct login flow again

--- components/UserAuthForm.jsx
+++ components/UserAuthForm.jsx
@@ -1,40 +1,37 @@
-"use client"
+"use client";
 
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
-import { Icons } from "@/components/icons"
-import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils";
+import { Icons } from "@/components/icons";
+import { Button } from "@/components/ui/button";
 // import { Input } from "@/components/ui/input"
 // import { Label } from "@/components/ui/label"
-import { signIn } from "next-auth/react"
-import config from "@/config"
-import { useRouter } from 'next/navigation'
+import { signIn } from "next-auth/react";
+import config from "@/config";
 export function UserAuthForm({ className, ...props }) {
-    const [isLoading, setIsLoading] = React.useState(false)
+  const [isLoading, setIsLoading] = React.useState(false);
 
-    const router = useRouter() 
-    // async function onSubmit(event) {
-    //     event.preventDefault()
-    //     setIsLoading(true)
+  //   async function onSubmit(event) {
+  //     event.preventDefault();
+  //     setIsLoading(true);
 
-    //     setTimeout(() => {
-    //         setIsLoading(false)
-    //     }, 3000)
-    // }
+  //     setTimeout(() => {
+  //       setIsLoading(false);
+  //     }, 3000);
+  //   }
 
-    const callbackUrl = config.auth.callbackUrl
+  const callbackUrl = config.auth.callbackUrl;
 
-    const handleGoogleSignIn = async () => {
-        setIsLoading(true)
-        // await signIn("google", { callbackUrl });
-        router.push('/dashboard')
-        setIsLoading(false)
-    };
+  const handleGoogleSignIn = async () => {
+    setIsLoading(true);
+    await signIn("google", { callbackUrl });
+    setIsLoading(false);
+  };
 
-    return (
-        <div className={cn("grid gap-6", className)} {...props}>
-            {/* <form onSubmit={onSubmit}>
+  return (
+    <div className={cn("grid gap-6", className)} {...props}>
+      {/* <form onSubmit={onSubmit}>
                 <div className="grid gap-2">
                     <div className="grid gap-1">
                         <Label className="sr-only" htmlFor="email">
@@ -58,24 +55,29 @@ export function UserAuthForm({ className, ...props }) {
                     </Button>
                 </div>
             </form> */}
-            <div className="relative">
-                <div className="absolute inset-0 flex items-center">
-                    <span className="w-full border-t" />
-                </div>
-                <div className="relative flex justify-center text-xs uppercase">
-                    <span className="bg-background px-2 text-muted-foreground">
-                        continue with
-                    </span>
-                </div>
-            </div>
-            <Button onClick={handleGoogleSignIn} variant="outline" type="button" disabled={isLoading}>
-                {isLoading ? (
-                    <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-                ) : (
-                    <Icons.google className="mr-2 h-4 w-4" />
-                )}{" "}
-                Google
-            </Button>
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t" />
         </div>
-    )
+        <div className="relative flex justify-center text-xs uppercase">
+          <span className="bg-background px-2 text-muted-foreground">
+            continue with
+          </span>
+        </div>
+      </div>
+      <Button
+        onClick={handleGoogleSignIn}
+        variant="outline"
+        type="button"
+        disabled={isLoading}
+      >
+        {isLoading ? (
+          <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+        ) : (
+          <Icons.google className="mr-2 h-4 w-4" />
+        )}{" "}
+        Google
+      </Button>
+    </div>
+  );
 }
